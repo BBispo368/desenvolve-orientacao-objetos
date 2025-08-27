6c306452ab5de120a981309ec50a8a6f1c3451d7
Breno Bispo <63658113+BBispo368@users.noreply.github.com>
2025-08-27 08:58:21 -0300

    Update Main.java

--- modulo2/Main.java
+++ modulo2/Main.java
@@ -1,19 +1,23 @@
 package modulo2;
 
-import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        // Dados que podem ser alterados conforme instrução
+        // Autor tradicional
         Autor autor = new Autor("Jessica Felix", "Brasileira");
+        // Autor que também é usuário (exemplo de uso do novo construtor)
+        Autor autorUsuario = new Autor("Mariana Silva", 34, "Brasileira", true);
+
         Livro livro = new Livro("Java for Beginners", autor, "Tecnologia");
         Usuario usuario = new Usuario("Lucas Rafael", 25);
 
-        // Simular livro não disponível (pode ser por empréstimo anterior)
+        // Criar um artigo conforme solicitado
+        Artigo artigo = new Artigo("Entendendo Compiladores", autor, "tecnologia", true);
+
+        // Simular livro não disponível
         livro.marcarComoEmprestado();
 
-        // Impressão conforme solicitado
         if (!livro.isDisponivel()) {
             System.out.println("O livro não está disponível");
         } else {
@@ -31,12 +35,17 @@ public class Main {
         System.out.println("Idade: " + usuario.getIdade());
         System.out.println();
 
-        // Criar datas (pode alterar)
         Date dataRetirada = new Date();
         Date dataDevolucao = new Date();
 
         System.out.println("Data de Retirada: " + dataRetirada);
         System.out.println();
         System.out.println("Data de Devolucao: " + dataDevolucao);
+        System.out.println();
+
+        // Exibir informações do artigo criado
+        System.out.println("Artigo: " + artigo.getTitulo());
+        System.out.println("Autor do Artigo: " + artigo.getAutor().getNome());
+        System.out.println("Publicado: " + (artigo.isPublicado() ? "Sim" : "Não"));
     }
 }
